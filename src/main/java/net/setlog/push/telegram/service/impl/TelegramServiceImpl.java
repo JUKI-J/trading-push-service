2b66a45b58c09abdc1531522a253fc39ccd80451
JUKI-J <jju.ki@hotmail.com>
2022-02-21 20:33:59 +0900

    +telegram api

--- src/main/java/net/setlog/push/telegram/service/impl/TelegramServiceImpl.java
+++ src/main/java/net/setlog/push/telegram/service/impl/TelegramServiceImpl.java
@@ -25,34 +25,26 @@ public class TelegramServiceImpl implements TelegramService {
 
     @Override
     public TelegramResultVO send(TelegramPushVO telegramPushVO) throws Exception {
-
         TelegramResultVO telegramResultVO = new TelegramResultVO();
-
         if (telegramConfiguration.isEnabled()) {
-
             String url = String.format("https://api.telegram.org/bot%s/sendmessage", telegramConfiguration.getBotToken());
-
             try {
                 RestTemplate restTemplate = new RestTemplate();
                 HttpHeaders headers = new HttpHeaders();
                 headers.setContentType(new MediaType("application","json", Charset.forName("UTF-8")));
-
                 UriComponents uriComponents = UriComponentsBuilder.fromHttpUrl(url)
                         .queryParam("chat_id", telegramConfiguration.getChatId())
                         .queryParam("text", telegramPushVO.getMessage())
                         .build()
                 ;
-
                 log.debug("-----UriComponents : {}", uriComponents.toUriString());
                 telegramResultVO = restTemplate.getForObject(uriComponents.toUriString(), TelegramResultVO.class);
-
             } catch (Exception e) {
                 log.error("Unhandled exception occurred while send Telegram.", e);
             }
         }else{
             log.error("Exception occurred before send Telegram. [ telegramConfiguration.isEnabled() : {} ]", telegramConfiguration.isEnabled());
         }
-
         log.info("-----telegramResultVO : {}", telegramResultVO);
         return telegramResultVO;
     }
